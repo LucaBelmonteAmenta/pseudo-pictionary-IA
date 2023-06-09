87c9dcbdd32bf73099971311e14627c21962ba56
Lucas Belmonte Amenta <lucasbelmonteamenta2.0@gamil.com>
2023-06-08 22:08:36 -0300

    controles extras de seguridad anti fallas en los inputs del juego base

--- controllers/InputController.py
+++ controllers/InputController.py
@@ -16,13 +16,18 @@ class InputController(Controller):
     #        Methods
     #-----------------------------------------------------------------------
 
-    def GamePart1(self, rondas, segundos, minutos, validacion):
+    def Continue(self, rondas, segundos, minutos, tamaño_resultados, validacion):
 
         if validacion:
-            data = {"segundos" : int(segundos), "minutos" : int(minutos), "numero_rondas" : int(rondas)}
+            if (segundos.isdigit() == False):
+                segundos="0"
+            if (minutos.isdigit() == False):
+                minutos="0"
+
+            data = {"segundos" : int(segundos), "minutos" : int(minutos), "numero_rondas" : int(rondas), "tamaño_resultados": int(tamaño_resultados)}
             Core.guardar_data_json(data)
 
-            c = Core.openController("countdownTimer")
+            c = Core.openController("game1")
             c.main()
 
 
